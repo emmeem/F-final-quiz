edee6e9ea955fcab5b1d16cdc04de4262f17e5db
emmeem <1124782639@qq.com>
2020-09-07 15:40:14 +0800

    add GroupList and StudentList

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount() {
+    
+  }
+
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <div className="GroupList">
+          <h2>分组列表</h2>
+          <button className="SpiltGroup">分组学员</button>
+        </div>
+        <div className="StudentList">
+          <h2>学员列表</h2>
+        </div>
       </div>
     );
   }
