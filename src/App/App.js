2bade60e2162a56d07d022a4d2d34150931594dd
emmeem <1124782639@qq.com>
2020-09-07 21:43:16 +0800

    fix splitGroup

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 class App extends Component {
   state = {
     StudentList: [],
+    Groups: {},
   }
 
   componentDidMount = () => {
@@ -22,21 +23,46 @@ class App extends Component {
           StudentList: jsonData,
         })
     })
-}
+  }
+  
+  splitGroup = () => {
+    URL = "http://localhost:8080/groups"
+    fetch(URL, {
+        method:"GET",
+    }).then(Response => {
+        if (Response.status === 200) {
+            return Response.json();
+        }
+        else {
+            Promise.reject();
+        }
+    }).then(jsonData => {
+        this.setState({
+          Groups: jsonData,
+        })
+    })
+  }
+
 
   render() {
     console.log(this.state.StudentList)
-
+    const result = this.state.StudentList.map(s =>{
+      return <div className="students">
+      <label>{s.id}.</label>
+      <label>{s.studentName}</label>
+      </div>
+    })
     return (
       <div data-testid="app" className="App">
         <div className="GroupList">
           <h2>分组列表</h2>
-          <button className="SpiltGroup">分组学员</button>
+          <button className="SpiltGroup" onClick="splitGroup()">分组学员</button>
         </div>
         <div className="StudentList">
           <h2>学员列表</h2>
-          <div>
-            
+          <div className="StudentInfo">
+            {result}
+            <button>+添加学员</button>
           </div>
         </div>
       </div>
