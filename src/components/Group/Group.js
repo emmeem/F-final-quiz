81215ecaafaa778ea919ad8992099c66c1aa5075
emmeem <1124782639@qq.com>
2020-09-24 20:57:22 +0800

    add group

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -11,6 +11,9 @@ class Group extends Component {
     return (
       <section className="group-list">
         <h2>分组列表</h2>
+          <button className="group-btn" onClick={this.getGroup}>
+            分组学员
+          </button>
       </section>
     );
   }
